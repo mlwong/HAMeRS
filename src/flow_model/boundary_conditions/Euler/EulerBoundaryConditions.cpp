c682b71bdb35961f424c3654b6993ab9fbd7ade4
Man Long Wong <wongml@corn38.stanford.edu>
2016-02-20 13:00:24 -0800

    modified EulerBoundaryCondition

--- src/flow_model/boundary_conditions/Euler/EulerBoundaryConditions.cpp
+++ src/flow_model/boundary_conditions/Euler/EulerBoundaryConditions.cpp
@@ -25,12 +25,12 @@ EulerBoundaryConditions::EulerBoundaryConditions(
         d_flow_model(flow_model),
         d_num_species(num_species),
         d_equation_of_state(equation_of_state),
-        d_density(NULL),
-        d_partial_density(NULL),
-        d_momentum(NULL),
-        d_total_energy(NULL),
-        d_mass_fraction(NULL),
-        d_volume_fraction(NULL),
+        d_density(nullptr),
+        d_partial_density(nullptr),
+        d_momentum(nullptr),
+        d_total_energy(nullptr),
+        d_mass_fraction(nullptr),
+        d_volume_fraction(nullptr),
         d_variables_set(false),
         d_num_ghosts_set(false)
 {
