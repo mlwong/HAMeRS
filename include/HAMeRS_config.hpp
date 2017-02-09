efc73848f4d3c4dc7355343cd113ef1fbefac02f
Man-Long <wongml@stanford.edu>
2017-02-09 00:14:55 -0800

    Modified CMakeFIle

--- include/HAMeRS_config.hpp
+++ include/HAMeRS_config.hpp
@@ -3,13 +3,13 @@
 
 #ifdef _OPENMP
 /* Enable SIMD */
-/* #undefine HAMERS_ENABLE_SIMD */
+/* #undef HAMERS_ENABLE_SIMD */
 #endif
 
 /* Enable assertion checking */
-#define HAMERS_DEBUG_CHECK_ASSERTIONS 1
+#define HAMERS_DEBUG_CHECK_ASSERTIONS
 
 /* Enable HAMeRS developer assertion checking */
-#define HAMERS_DEBUG_CHECK_DEV_ASSERTIONS 1
+#define HAMERS_DEBUG_CHECK_DEV_ASSERTIONS
 
 #endif /* HAMERS_CONFIG_HPP */
