73fcbf22a6e692472ebb8db2efdbb58693ddeead
Man-Long <wongml@stanford.edu>
2017-04-03 17:51:21 -0700

    Added new stuff for flow model statistics utilities

--- include/flow/flow_models/four-eqn_conservative/FlowModelStatisticsUtilitiesFourEqnConservative.hpp
+++ include/flow/flow_models/four-eqn_conservative/FlowModelStatisticsUtilitiesFourEqnConservative.hpp
@@ -36,6 +36,97 @@ class FlowModelStatisticsUtilitiesFourEqnConservative: public FlowModelStatistic
             const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
             const boost::shared_ptr<hier::VariableContext>& data_context);
         
+    private:
+        /*
+         * Output mixing width in x-direction to a file.
+         */
+        void
+        outputMixingWidthInXDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output mixing width in y-direction to a file.
+         */
+        void
+        outputMixingWidthInYDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output mixing width in z-direction to a file.
+         */
+        void
+        outputMixingWidthInZDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output mixedness in x-direction to a file.
+         */
+        void
+        outputMixednessInXDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output mixedness in y-direction to a file.
+         */
+        void
+        outputMixednessInYDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output mixedness in z-direction to a file.
+         */
+        void
+        outputMixednessInZDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output enstrophy integrated to a file.
+         */
+        void
+        outputEnstrophyIntegrated(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output TKE integrated with assumed homogeneity in x-direction to a file.
+         */
+        void
+        outputTKEIntegratedWithHomogeneityInXDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output TKE integrated with assumed homogeneity in y-direction to a file.
+         */
+        void
+        outputTKEIntegratedWithHomogeneityInYDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
+        /*
+         * Output TKE integrated with assumed homogeneity in z-direction to a file.
+         */
+        void
+        outputTKEIntegratedWithHomogeneityInZDirection(
+            const std::string& filename_statistics,
+            const boost::shared_ptr<hier::PatchHierarchy>& patch_hierarchy,
+            const boost::shared_ptr<hier::VariableContext>& data_context);
+        
 };
 
 #endif /* FLOW_MODEL_STATISTICS_UTILITIES_FOUR_EQN_CONSERVATIVE_HPP */
