d82780ed75258be4e99b1098e76d480e112c43bb
Man-Long Wong <wongml@stanford.edu>
2017-08-24 17:27:34 +0800

    Update install-hdf5.sh

--- travis/install-hdf5.sh
+++ travis/install-hdf5.sh
@@ -3,5 +3,6 @@
 git clone --branch hdf5_1_8_14 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_14
 cd hdf5_1_8_14
 ./configure --prefix=${HOME}/hdf5
-make
+make -s
 make install
+export HDF5_ROOT=${HOME}/hdf5
