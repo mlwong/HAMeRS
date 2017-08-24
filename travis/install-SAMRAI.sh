54ef316eeef4ce3b3bc659c210885d0bbef13d05
Man-Long Wong <wongml@stanford.edu>
2017-08-24 15:00:13 +0800

    Update install-SAMRAI.sh

--- travis/install-SAMRAI.sh
+++ travis/install-SAMRAI.sh
@@ -5,8 +5,10 @@ set -x
 
 cd ${HOME}
 wget https://computation.llnl.gov/projects/samrai/download/SAMRAI-v3.11.2.tar.gz
-tar zxvf SAMRAI-v3.11.2.tar.gz
-cd SAMRAI
+gunzip SAMRAI-v3.11.2.tar.gz
+mkdir SAMRAI-v3.11.2
+cd SAMRAI-v3.11.2
+tar xvf ../SAMRAI-v3.11.2.tar
 mkdir objs
 cd objs
 sh ../SAMRAI/configure --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost --with-hdf5
