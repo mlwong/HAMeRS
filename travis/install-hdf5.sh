4528248c6f6f59b722b1033cd2843a4c27ea457c
Man-Long Wong <wongml@stanford.edu>
2017-08-24 16:49:58 +0800

    Update install-hdf5.sh

--- travis/install-hdf5.sh
+++ travis/install-hdf5.sh
@@ -5,6 +5,3 @@ cd hdf5_1_8_14
 ./configure --prefix=${HOME}/hdf5
 make
 make install
-export HDF5_ROOT=${HOME}/hdf5
-export HDF5_LIBRARIES=${HOME}/hdf5/lib
-export HDF5_INCLUDE_DIRS=${HOME}/hdf5/include
