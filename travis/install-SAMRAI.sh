29a945739e05bfc72e7a21994a649cbc3a5a5267
Man-Long Wong <wongml@stanford.edu>
2017-08-24 19:45:39 +0800

    Update install-SAMRAI.sh

--- travis/install-SAMRAI.sh
+++ travis/install-SAMRAI.sh
@@ -9,5 +9,5 @@ tar xvf ../SAMRAI-v3.11.2.tar > /dev/null 2>&1
 mkdir objs
 cd objs
 sh ../SAMRAI/configure --prefix=$SAMRAI_ROOT --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost --with-hdf5=$HDF5_ROOT
-travis_wait 60 make library > /dev/null 2>&1
+make library
 make install
