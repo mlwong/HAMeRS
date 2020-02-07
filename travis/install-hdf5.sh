162e4738f4e153f8cc2e1ee4466c10913fefc582
Man Long Wong <wongml@stanford.edu>
2020-02-06 22:20:32 -0800

    Fixing install-hdf5 in travis

--- travis/install-hdf5.sh
+++ travis/install-hdf5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone --branch hdf5_1_8_14 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_14
+git clone --branch hdf5_1_8_14 https://git.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8
 cd hdf5_1_8_14
 ./configure --prefix=$HDF5_ROOT --without-zlib
 make > /dev/null 2>&1
