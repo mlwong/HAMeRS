d8d8342ef238e2dfe5c5ca03e4da64d93c9047ed
Man Long Wong <wongml@stanford.edu>
2020-10-24 17:47:17 -0700

    Adding input and initial conditions for smooth single-model RTI

--- problems/Navier-Stokes/initial_conditions/RayleighTaylorInstabilitySingleMode2D.cpp
+++ problems/Navier-Stokes/initial_conditions/RayleighTaylorInstabilitySingleMode2D.cpp
@@ -10,13 +10,18 @@ NavierStokesInitialConditions::initializeDataOnPatch(
     const double data_time,
     const bool initial_time)
 {
+    // Follow Reckinger, Scott J., Daniel Livescu, and Oleg V. Vasilyev.
+    // "Comprehensive numerical methodology for direct numerical simulations of compressible Rayleigh–Taylor instability."
+    // Journal of Computational Physics 313 (2016): 181-208.
+    // Note that the sign of gravity in the paper is flipped.
     NULL_USE(data_time);
     
-    if (d_project_name != "2D Rayleigh-Taylor instability")
+    if ((d_project_name != "2D discontinuous Rayleigh-Taylor instability") && (d_project_name != "2D smooth Rayleigh-Taylor instability"))
     {
         TBOX_ERROR(d_object_name
             << ": "
-            << "Can only initialize data for 'project_name' = '2D Rayleigh-Taylor instability'!\n"
+            << "Can only initialize data for 'project_name' = '2D discontinuous Rayleigh-Taylor instability' or "
+            << "'2D smooth Rayleigh-Taylor instability'!\n"
             << "'project_name' = '"
             << d_project_name
             << "' is given."
@@ -65,7 +70,7 @@ NavierStokesInitialConditions::initializeDataOnPatch(
         const hier::IntVector patch_dims = patch_box.numberCells();
         
         /*
-         * Initialize data for a 2D Rayleigh-Taylor instability problem.
+         * Initialize data for a 2D Rayleigh-Taylor instability problem (At = 0.04, M = 0.3).
          */
         
         boost::shared_ptr<pdat::CellData<double> > partial_density = conservative_variables[0];
@@ -78,64 +83,136 @@ NavierStokesInitialConditions::initializeDataOnPatch(
         double* rho_v   = momentum->getPointer(1);
         double* E       = total_energy->getPointer(0);
         
-        const double gamma = double(7)/double(5);
+        const double gamma = double(7)/double(5); // assume both gases have the same ratio of specific heat ratios
         // const double gamma_0 = double(7)/double(5);
         // const double gamma_1 = double(7)/double(5);
         
-        const double lambda = 701.53278340668;
-        const double eta_0  = 0.01*lambda;
+        const double lambda = 701.53278340668; // wavelength of single-mode perturbation
+        // const double eta_0  = 0.01*lambda;     // 1% perturbation
+        const double eta_0  = 0.0*lambda;      // no perturbation
         
-        const double W_1 = 0.03328;
-        const double W_2 = 0.03072;
+        const double W_1 = 0.03328; // molecular weight of heavier gas
+        const double W_2 = 0.03072; // molecular weight of lighter gas
         
-        const double p_i = 100000.0;
-        const double T_0 = 300.0;
+        const double p_i = 100000.0; // interface pressure
+        const double T_0 = 300.0;    // background temperature
         
-        const double g   = 10.0;
-        const double R_u = 8.31446261815324;
+        const double g   = 10.0; // gravity
         
-        const double R_1 = R_u/W_1;
-        const double R_2 = R_u/W_2;
+        const double R_u = 8.31446261815324; // universal gas constant
+        const double R_1 = R_u/W_1;          // gas constant of heavier gas
+        const double R_2 = R_u/W_2;          // gas constant of lighter gas
         
         // const double rho_i = p_i/(R_u*T_0)*(W_1 + W_2)/2.0;
         
-        for (int j = 0; j < patch_dims[1]; j++)
+        if (d_project_name == "2D discontinuous Rayleigh-Taylor instability")
         {
-            for (int i = 0; i < patch_dims[0]; i++)
+            for (int j = 0; j < patch_dims[1]; j++)
             {
-                // Compute index into linear data array.
-                int idx_cell = i + j*patch_dims[0];
-                
-                // Compute the coordinates.
-                double x[2];
-                x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
-                x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
-                
-                // const double delta = eta_0*cos(2.0*M_PI/lambda*x[1]);
-                const double delta = 0.0;
-                
-                if (x[0] < delta) // heavier fluid
+                for (int i = 0; i < patch_dims[0]; i++)
                 {
-                    const double rho = p_i/(R_1*T_0)*exp((g*x[0])/(R_1*T_0));
-                    rho_Y_0[idx_cell] = rho;
-                    rho_Y_1[idx_cell] = 0.0;
+                    // Compute index into linear data array.
+                    int idx_cell = i + j*patch_dims[0];
                     
-                    const double p = p_i*exp((g*x[0])/(R_1*T_0));
+                    // Compute the coordinates.
+                    double x[2];
+                    x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
+                    x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
                     
-                    const double u = 0.0;
-                    const double v = 0.0;
+                    const double eta = eta_0*cos(2.0*M_PI/lambda*x[1]);
                     
-                    rho_u[idx_cell] = rho*u;
-                    rho_v[idx_cell] = rho*v;
-                    E[idx_cell]     = p/(gamma - double(1)) + double(1)/double(2)*rho*(u*u + v*v);
+                    if (x[0] < eta) // heavier fluid
+                    {
+                        const double rho = p_i/(R_1*T_0)*exp((g*x[0])/(R_1*T_0));
+                        rho_Y_0[idx_cell] = rho;
+                        rho_Y_1[idx_cell] = 0.0;
+                        
+                        const double p = p_i*exp((g*x[0])/(R_1*T_0));
+                        
+                        const double u = 0.0;
+                        const double v = 0.0;
+                        
+                        rho_u[idx_cell] = rho*u;
+                        rho_v[idx_cell] = rho*v;
+                        E[idx_cell]     = p/(gamma - double(1)) + double(1)/double(2)*rho*(u*u + v*v);
+                    }
+                    else // lighter fluid
+                    {
+                        const double rho = p_i/(R_2*T_0)*exp((g*x[0])/(R_2*T_0));
+                        rho_Y_0[idx_cell] = 0.0;
+                        rho_Y_1[idx_cell] = rho;
+                        
+                        const double p = p_i*exp((g*x[0])/(R_2*T_0));
+                        
+                        const double u = 0.0;
+                        const double v = 0.0;
+                        
+                        rho_u[idx_cell] = rho*u;
+                        rho_v[idx_cell] = rho*v;
+                        E[idx_cell]     = p/(gamma - double(1)) + double(1)/double(2)*rho*(u*u + v*v);
+                    }
                 }
-                else // lighter fluid
+            }
+        }
+        else if (d_project_name == "2D smooth Rayleigh-Taylor instability")
+        {
+            const double delta = 0.1*lambda; // characteristic length of interface.
+            
+            for (int j = 0; j < patch_dims[1]; j++)
+            {
+                for (int i = 0; i < patch_dims[0]; i++)
                 {
-                    const double rho = p_i/(R_2*T_0)*exp((g*x[0])/(R_2*T_0));
-                    rho_Y_0[idx_cell] = 0.0;
-                    rho_Y_1[idx_cell] = rho;
+                    // Compute index into linear data array.
+                    int idx_cell = i + j*patch_dims[0];
                     
-                    const double p = p_i*exp((g*x[0])/(R_2*T_0));
+                    // Compute the coordinates.
+                    double x[2];
+                    x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
+                    x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
+                    
+                    const double X_2_H = 0.5*(1.0 + erf(x[0]/delta)); // mass fraction of second species (Y_2)
+                    const double R_H = R_1*(1.0 - X_2_H) + X_2_H*R_2;
+                    const double dX_2_H_dx = 1.0/(delta*sqrt(M_PI))*exp(-(x[0]/delta)*(x[0]/delta));
+                    const double dlnR_H_dx = (R_2 - R_1)*dX_2_H_dx;
+                    const double p_H = p_i*exp(g/(R_H*T_0)*(x[0] - 0.5*delta*delta*dlnR_H_dx));
+                    const double rho_H = p_H/(R_H*T_0);
+                    
+                    const double eta = eta_0*cos(2.0*M_PI/lambda*x[1]);
+                    
+                    const double X_2 = 0.5*(1.0 + erf((x[0] - eta)/delta)); // mass fraction of second species (Y_2)
+                    
+                    double rho, p;
+                    
+                    rho = rho_H;
+                    p   = p_H;
+                    
+                    // if (x[0] < eta)
+                    // {
+                    //     const double p_1_H   = p_i*exp((g*x[0])/(R_1*T_0));
+                    //     const double rho_1_H = p_i/(R_1*T_0)*exp((g*x[0])/(R_1*T_0));
+                    //     
+                    //     const double p_1   = p_i*exp((g*x[0])/(R_1*T_0));
+                    //     const double rho_1 = p_i/(R_1*T_0)*exp((g*x[0])/(R_1*T_0));
+                    //     
+                    //     p   = p_1*p_H/p_1_H;
+                    //     rho = rho_1*rho_H/rho_1_H;
+                    //     
+                    //     
+                    // }
+                    // else
+                    // {
+                    //     const double p_2_H   = p_i*exp((g*x[0])/(R_2*T_0));
+                    //     const double rho_2_H = p_i/(R_2*T_0)*exp((g*x[0])/(R_2*T_0));
+                    //     
+                    //     const double p_2   = p_i*exp((g*x[0])/(R_2*T_0));
+                    //     const double rho_2 = p_i/(R_2*T_0)*exp((g*x[0])/(R_2*T_0));
+                    //     
+                    //     p   = p_2*p_H/p_2_H;
+                    //     rho = rho_2*rho_H/rho_2_H;
+                    // }
+                    
+                    rho_Y_0[idx_cell] = rho*(1.0 - X_2);
+                    rho_Y_1[idx_cell] = rho*X_2;
                     
                     const double u = 0.0;
                     const double v = 0.0;
