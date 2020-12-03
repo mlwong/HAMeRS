c92451fc012b766b98a975a898cd7dfcb6c07d6c
Denis Aslangil <denis.aslangil@gmail.com>
2020-12-02 19:01:44 -0700

    Nonreflective BCs are added at y- and z-directions for multimaterial 3D

--- problems/Navier-Stokes/initial_conditions/VortexLeavingDomain3D.cpp
+++ problems/Navier-Stokes/initial_conditions/VortexLeavingDomain3D.cpp
@@ -284,7 +284,7 @@ NavierStokesInitialConditions::initializeDataOnPatch(
                 const double L       = double(1.0);
                 const double Gamma_v = double(0.024);
                 const double R_v     = L/double(10);
-                const double Ma      = -double(0.283);
+                const double Ma      = double(0.283);
                 
                 const double x_v = double(0);
                 const double y_v = double(0);
@@ -342,11 +342,138 @@ NavierStokesInitialConditions::initializeDataOnPatch(
             }
             else if (d_project_name == "3D vortex leaving domain in y-direction")
             {
-                TBOX_ERROR("It is not implemented yet.");
+                const double c_p_0 = double(7);
+                const double c_p_1 = double(7);
+                
+                const double c_v_0 = double(5);
+                const double c_v_1 = double(5);
+                
+                const double L       = double(1.0);
+                const double Gamma_v = double(0.024);
+                const double R_v     = L/double(10);
+                const double Ma      = double(0.283);
+                
+                const double x_v = double(0);
+                const double y_v = double(0);
+                
+                const double rho_inf = double(1);
+                const double Y_0_inf = double(1)/double(2);
+                const double Y_1_inf = double(1)/double(2);
+                
+                const double c_p_mixture = Y_0_inf*c_p_0 + Y_1_inf*c_p_1;
+                const double c_v_mixture = Y_0_inf*c_v_0 + Y_1_inf*c_v_1;
+                
+                const double gamma_mixture = c_p_mixture/c_v_mixture;
+                
+                const double p_inf = double(1)/gamma_mixture;
+                const double v_inf = Ma*sqrt(gamma_mixture*p_inf/rho_inf);
+                
+                const double c = sqrt(gamma_mixture*p_inf/rho_inf);
+                const double Gamma_normalized = Gamma_v/(c*R_v);
+                
+                for (int k = 0; k < patch_dims[2]; k++)
+                {
+                    for (int j = 0; j < patch_dims[1]; j++)
+                    {
+                        for (int i = 0; i < patch_dims[0]; i++)
+                        {
+                            // Compute index into linear data array.
+                            int idx_cell = i + j*patch_dims[0] + k*patch_dims[0]*patch_dims[1];
+                            
+                            // Compute the coordinates.
+                            double x[3];
+                            x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
+                            x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
+                            x[2] = patch_xlo[2] + (double(k) + double(1)/double(2))*dx[2];
+                            
+                            const double r = sqrt(pow(x[0] - x_v, 2) + pow(x[1] - y_v, 2));
+                            
+                            const double exp_factor = exp(-pow(r/R_v, 2));
+                            const double exp_factor_half  = sqrt(exp_factor);
+                            
+                            const double p_vortex   = p_inf*exp(-double(1)/double(2)*gamma_mixture*Gamma_normalized*Gamma_normalized*exp_factor);
+                            const double rho_vortex = rho_inf/p_inf*p_vortex;
+                            
+                            const double u_vortex =         exp_factor_half * (x[1] - y_v) * Gamma_v/pow(R_v, 2);
+                            const double v_vortex = v_inf - exp_factor_half * (x[0] - x_v) * Gamma_v/pow(R_v, 2);
+                            
+                            rho_Y_0[idx_cell] = rho_vortex*Y_0_inf;
+                            rho_Y_1[idx_cell] = rho_vortex*Y_1_inf;
+                            rho_u[idx_cell]   = rho_vortex*u_vortex;
+                            rho_v[idx_cell]   = rho_vortex*v_vortex;
+                            rho_w[idx_cell]   = double(0);
+                            E[idx_cell]       = p_vortex/(gamma_mixture - double(1)) + double(1)/double(2)*rho_vortex*(u_vortex*u_vortex + v_vortex*v_vortex);
+                        }
+                    }
+                }
             }
             else if (d_project_name == "3D vortex leaving domain in z-direction")
             {
-                TBOX_ERROR("It is not implemented yet.");
+                
+                const double c_p_0 = double(7);
+                const double c_p_1 = double(7);
+                
+                const double c_v_0 = double(5);
+                const double c_v_1 = double(5);
+                
+                const double L       = double(1.0);
+                const double Gamma_v = double(0.024);
+                const double R_v     = L/double(10);
+                const double Ma      = double(0.283);
+                
+                const double x_v = double(0);
+                const double z_v = double(0);
+                
+                const double rho_inf = double(1);
+                const double Y_0_inf = double(1)/double(2);
+                const double Y_1_inf = double(1)/double(2);
+                
+                const double c_p_mixture = Y_0_inf*c_p_0 + Y_1_inf*c_p_1;
+                const double c_v_mixture = Y_0_inf*c_v_0 + Y_1_inf*c_v_1;
+                
+                const double gamma_mixture = c_p_mixture/c_v_mixture;
+                
+                const double p_inf = double(1)/gamma_mixture;
+                const double w_inf = Ma*sqrt(gamma_mixture*p_inf/rho_inf);
+                
+                const double c = sqrt(gamma_mixture*p_inf/rho_inf);
+                const double Gamma_normalized = Gamma_v/(c*R_v);
+                
+                for (int k = 0; k < patch_dims[2]; k++)
+                {
+                    for (int j = 0; j < patch_dims[1]; j++)
+                    {
+                        for (int i = 0; i < patch_dims[0]; i++)
+                        {
+                            // Compute index into linear data array.
+                            int idx_cell = i + j*patch_dims[0] + k*patch_dims[0]*patch_dims[1];
+                            
+                            // Compute the coordinates.
+                            double x[3];
+                            x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
+                            x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
+                            x[2] = patch_xlo[2] + (double(k) + double(1)/double(2))*dx[2];
+                            
+                            const double r = sqrt(pow(x[0] - x_v, 2) + pow(x[2] - z_v, 2));
+                            
+                            const double exp_factor = exp(-pow(r/R_v, 2));
+                            const double exp_factor_half  = sqrt(exp_factor);
+                            
+                            const double p_vortex   = p_inf*exp(-double(1)/double(2)*gamma_mixture*Gamma_normalized*Gamma_normalized*exp_factor);
+                            const double rho_vortex = rho_inf/p_inf*p_vortex;
+                            
+                            const double u_vortex =         exp_factor_half * (x[2] - z_v) * Gamma_v/pow(R_v, 2);
+                            const double w_vortex = w_inf - exp_factor_half * (x[0] - x_v) * Gamma_v/pow(R_v, 2);
+                            
+                            rho_Y_0[idx_cell] = rho_vortex*Y_0_inf;
+                            rho_Y_1[idx_cell] = rho_vortex*Y_1_inf;
+                            rho_u[idx_cell]   = rho_vortex*u_vortex;
+                            rho_v[idx_cell]   = double(0);
+                            rho_w[idx_cell]   = rho_vortex*w_vortex;
+                            E[idx_cell]       = p_vortex/(gamma_mixture - double(1)) + double(1)/double(2)*rho_vortex*(u_vortex*u_vortex + w_vortex*w_vortex);
+                        }
+                    }
+                }
             }
         }
     }
