7243d214b86834a4721ddf38959d36f4e3407c9c
Man-Long (Norman) Wong <manlong89@gmail.com>
2021-06-01 21:20:01 -0700

    Update install-SAMRAI_with_Boost.sh

--- travis/install-SAMRAI_with_Boost.sh
+++ travis/install-SAMRAI_with_Boost.sh
@@ -5,9 +5,9 @@ wget https://github.com/LLNL/SAMRAI/archive/refs/tags/v-3-11-2.tar.gz
 gunzip v-3-11-2.tar.gz
 mkdir SAMRAI-v3.11.2
 cd SAMRAI-v3.11.2
-tar xvf ../SAMRAI-v3.11.2.tar > /dev/null 2>&1
+tar xvf ../v-3-11-2.tar > /dev/null 2>&1
 mkdir objs
 cd objs
-sh ../SAMRAI/configure --prefix=$SAMRAI_ROOT_WITH_BOOST --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost --with-hdf5=$HDF5_ROOT
+sh ../SAMRAI-v-3-11-2/configure --prefix=$SAMRAI_ROOT_WITH_BOOST --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost --with-hdf5=$HDF5_ROOT
 make library
 make install
