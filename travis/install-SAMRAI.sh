98e0750a8c8f0be4a1070ea4e777367e8d704e34
Man-Long (Norman) Wong <manlong89@gmail.com>
2021-06-05 13:27:14 -0700

    Update install-SAMRAI.sh

--- travis/install-SAMRAI.sh
+++ travis/install-SAMRAI.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 cd ${HOME}
-wget https://github.com/LLNL/SAMRAI/archive/refs/tags/v-4-0-2.tar.gz
-gunzip v-4-0-2.tar.gz
-mkdir SAMRAI-v4.0.2
-cd SAMRAI-v4.0.2
-tar xvf ../v-4-0-2.tar > /dev/null 2>&1
+wget https://github.com/LLNL/SAMRAI/releases/download/v-4-1-0/SAMRAI-v4.1.0.tar.gz
+gunzip SAMRAI-v4.1.0.tar.gz
+mkdir SAMRAI-v4.1.0
+cd SAMRAI-v4.1.0
+tar xvf ../SAMRAI-v4.1.0.tar > /dev/null 2>&1
 mkdir objs
 cd objs
-cmake ../SAMRAI-v-4-0-2 -DCMAKE_INSTALL_PREFIX=$SAMRAI_ROOT_NO_BOOST -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CC_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif77 -DENABLE_HDF5=ON -DHDF5_DIR=$HDF5_ROOT
+cmake ../SAMRAI -DCMAKE_INSTALL_PREFIX=$SAMRAI_ROOT_NO_BOOST -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CC_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif77 -DENABLE_HDF5=ON -DHDF5_DIR=$HDF5_ROOT
 make
 make install
