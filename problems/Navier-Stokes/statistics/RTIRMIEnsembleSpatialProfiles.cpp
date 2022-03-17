f0b1a9a45e95a61c433a115189161335c5dfd935
Man Long Wong <wongml@stanford.edu>
2022-03-17 01:02:52 -0500

    added mass fraction variance ensemble spatial profile

--- problems/Navier-Stokes/statistics/RTIRMIEnsembleSpatialProfiles.cpp
+++ problems/Navier-Stokes/statistics/RTIRMIEnsembleSpatialProfiles.cpp
@@ -15,15 +15,27 @@ class EnsembleStatisticsRTIRMI: public EnsembleStatistics
             EnsembleStatistics(
                 object_name)
         {
+            Y_0_avg_computed    = false;
+            Y_0_sq_avg_computed = false;
         }
         
         void clearAllData()
         {
             Y_0_avg_realizations.clear();
+            Y_0_sq_avg_realizations.clear();
+            
+            Y_0_avg_computed    = false;
+            Y_0_sq_avg_computed = false;
         }
         
+        // Scratch arrays.
         // Number of realizalizations; number of cells.
         std::vector<std::vector<double> > Y_0_avg_realizations;
+        std::vector<std::vector<double> > Y_0_sq_avg_realizations;
+        
+        // Whether the scratch arrays are filled.
+        bool Y_0_avg_computed;
+        bool Y_0_sq_avg_computed;
         
     private:
         
@@ -64,7 +76,15 @@ class RTIRMISpatialProfilesUtilities
         void
         computeAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
             const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
-            const HAMERS_SHARED_PTR<hier::VariableContext>& data_context) const;
+            const HAMERS_SHARED_PTR<hier::VariableContext>& data_context);
+        
+        /*
+         * Compute mass fraction variance with assumed homogeneity in y-direction (2D) or yz-plane (3D).
+         */
+        void
+        computeMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+            const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
+            const HAMERS_SHARED_PTR<hier::VariableContext>& data_context);
         
         /*
          * Output ensemble averaged mass fraction with assumed homogeneity in y-direction (2D) or yz-plane (3D)
@@ -76,6 +96,22 @@ class RTIRMISpatialProfilesUtilities
             const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
             const HAMERS_SHARED_PTR<hier::VariableContext>& data_context,
             const double output_time) const;
+        
+        /*
+         * Output ensemble variance of mass fraction with assumed homogeneity in y-direction (2D) or yz-plane (3D)
+         * to a file.
+         */
+        void
+        outputEnsembleMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+            const std::string& stat_dump_filename,
+            const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
+            const HAMERS_SHARED_PTR<hier::VariableContext>& data_context,
+            const double output_time) const;
+        
+        /*
+         * Store ensemble statistics.
+         */
+        HAMERS_SHARED_PTR<EnsembleStatisticsRTIRMI> d_ensemble_statistics;
         
     private:
         /*
@@ -138,11 +174,6 @@ class RTIRMISpatialProfilesUtilities
          */
         const int d_num_ghosts_derivative;
         
-        /*
-         * Store ensemble statistics.
-         */
-        HAMERS_SHARED_PTR<EnsembleStatisticsRTIRMI> d_ensemble_statistics;
-        
 };
 
 
@@ -152,7 +183,7 @@ class RTIRMISpatialProfilesUtilities
 void
 RTIRMISpatialProfilesUtilities::computeAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
     const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
-    const HAMERS_SHARED_PTR<hier::VariableContext>& data_context) const
+    const HAMERS_SHARED_PTR<hier::VariableContext>& data_context)
 {
     if (d_num_species != 2)
     {
@@ -188,6 +219,67 @@ RTIRMISpatialProfilesUtilities::computeAveragedMassFractionWithHomogeneityInYDir
     
     std::vector<std::vector<double> >& Y_0_avg_realizations = d_ensemble_statistics->Y_0_avg_realizations;
     Y_0_avg_realizations.push_back(Y_0_avg_global);
+    
+    d_ensemble_statistics->Y_0_avg_computed = true;
+}
+
+
+/*
+ * Compute mass fraction variance with assumed homogeneity in y-direction (2D) or yz-plane (3D).
+ */
+void
+RTIRMISpatialProfilesUtilities::computeMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+    const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
+    const HAMERS_SHARED_PTR<hier::VariableContext>& data_context)
+{
+    if (d_num_species != 2)
+    {
+        TBOX_ERROR(d_object_name
+            << ": "
+            << "'MASS_FRACTION_VAR' can be computed with two species only."
+            << std::endl);
+    }
+    
+    if (d_flow_model.expired())
+    {
+        TBOX_ERROR(d_object_name
+            << ": "
+            << "The object is not setup yet!"
+            << std::endl);
+    }
+    
+    const tbox::SAMRAI_MPI& mpi(tbox::SAMRAI_MPI::getSAMRAIWorld());
+    
+    HAMERS_SHARED_PTR<FlowModel> flow_model_tmp = d_flow_model.lock();
+    
+    FlowModelMPIHelperAverage MPI_helper_average = FlowModelMPIHelperAverage(
+        "MPI_helper_average",
+        d_dim,
+        d_grid_geometry,
+        patch_hierarchy,
+        flow_model_tmp);
+    
+    std::vector<std::string> quantity_names;
+    std::vector<int> component_indices;
+    
+    quantity_names.push_back("MASS_FRACTIONS");
+    component_indices.push_back(0);
+    
+    quantity_names.push_back("MASS_FRACTIONS");
+    component_indices.push_back(0);
+    
+    std::vector<double> Y_0_sq_avg_global = MPI_helper_average.getAveragedQuantityWithInhomogeneousXDirection(
+        quantity_names,
+        component_indices,
+        data_context);
+    
+    quantity_names.clear();
+    component_indices.clear();
+    
+    std::vector<std::vector<double> >& Y_0_sq_avg_realizations = d_ensemble_statistics->Y_0_sq_avg_realizations;
+    Y_0_sq_avg_realizations.push_back(Y_0_sq_avg_global);
+    
+    d_ensemble_statistics->Y_0_sq_avg_computed = true;
 }
 
 
@@ -248,6 +340,81 @@ RTIRMISpatialProfilesUtilities::outputEnsembleAveragedMassFractionWithHomogeneit
         
         f_out.write((char*)&output_time, sizeof(double));
         f_out.write((char*)&Y_0_avg_global[0], sizeof(double)*Y_0_avg_global.size());
+        
+        f_out.close();
+    }
+}
+
+
+/*
+ * Output ensemble variance of mass fraction with assumed homogeneity in y-direction (2D) or yz-plane (3D)
+ * to a file.
+ */
+void
+RTIRMISpatialProfilesUtilities::outputEnsembleMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+    const std::string& stat_dump_filename,
+    const HAMERS_SHARED_PTR<hier::PatchHierarchy>& patch_hierarchy,
+    const HAMERS_SHARED_PTR<hier::VariableContext>& data_context,
+    const double output_time) const
+{
+#ifdef HAMERS_DEBUG_CHECK_ASSERTIONS
+    TBOX_ASSERT(!stat_dump_filename.empty());
+#endif
+    
+    const tbox::SAMRAI_MPI& mpi(tbox::SAMRAI_MPI::getSAMRAIWorld());
+    
+    std::ofstream f_out;
+    
+    /*
+     * Output the spatial profile (only done by process 0).
+     */
+    
+    if (mpi.getRank() == 0)
+    {
+        f_out.open(stat_dump_filename, std::ios_base::app | std::ios::out | std::ios::binary);
+        if (!f_out.is_open())
+        {
+            TBOX_ERROR(d_object_name
+                << ": "
+                << "Failed to open file to output statistics!"
+                << std::endl);
+        }
+        
+        const std::vector<std::vector<double> >& Y_0_avg_realizations =
+            d_ensemble_statistics->Y_0_avg_realizations;
+        
+        const std::vector<std::vector<double> >& Y_0_sq_avg_realizations = 
+            d_ensemble_statistics->Y_0_sq_avg_realizations;
+        
+        const int num_realizations = static_cast<int>(Y_0_avg_realizations.size());
+        
+        TBOX_ASSERT(d_ensemble_statistics->getNumberOfEnsembles() == num_realizations);
+        TBOX_ASSERT(num_realizations > 0);
+        TBOX_ASSERT(num_realizations == static_cast<int>(Y_0_sq_avg_realizations.size()));
+        
+        const int num_cells = static_cast<int>(Y_0_avg_realizations[0].size());
+        const double weight = double(1)/double(num_realizations);
+        
+        std::vector<double> Y_0_avg_global(num_cells, double(0));
+        std::vector<double> Y_0_sq_avg_global(num_cells, double(0));
+        
+        for (int ri = 0; ri < num_realizations; ri++)
+        {
+            for (int i = 0; i < num_cells; i++)
+            {
+                Y_0_avg_global[i] += weight*Y_0_avg_realizations[ri][i];
+                Y_0_sq_avg_global[i] += weight*Y_0_sq_avg_realizations[ri][i];
+            }
+        }
+        
+        std::vector<double> Y_0_var_global(num_cells, double(0));
+        for (int i = 0; i < num_cells; i++)
+        {
+            Y_0_var_global[i] = Y_0_sq_avg_global[i]  - Y_0_avg_global[i]*Y_0_avg_global[i];
+        }
+        
+        f_out.write((char*)&output_time, sizeof(double));
+        f_out.write((char*)&Y_0_var_global[0], sizeof(double)*Y_0_var_global.size());
         
         f_out.close();
     }
@@ -316,6 +483,9 @@ FlowModelStatisticsUtilitiesFourEqnConservative::computeStatisticalQuantities(
             d_equation_of_thermal_conductivity_mixing_rules,
             HAMERS_DYNAMIC_POINTER_CAST<EnsembleStatisticsRTIRMI>(d_ensemble_statistics)));
     
+    rti_rmi_spatial_profiles_utilities->d_ensemble_statistics->Y_0_avg_computed    = false;
+    rti_rmi_spatial_profiles_utilities->d_ensemble_statistics->Y_0_sq_avg_computed = false;
+    
     // Loop over statistical quantities.
     for (int qi = 0; qi < static_cast<int>(d_statistical_quantities.size()); qi++)
     {
@@ -324,10 +494,31 @@ FlowModelStatisticsUtilitiesFourEqnConservative::computeStatisticalQuantities(
         
         if (statistical_quantity_key == "MASS_FRACTION_AVG")
         {
-            rti_rmi_spatial_profiles_utilities->
-                computeAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
-                    patch_hierarchy,
-                    data_context);
+            if (!(rti_rmi_spatial_profiles_utilities->d_ensemble_statistics->Y_0_avg_computed))
+            {
+                rti_rmi_spatial_profiles_utilities->
+                    computeAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
+                        patch_hierarchy,
+                        data_context);
+            }
+        }
+        else if (statistical_quantity_key == "MASS_FRACTION_VAR")
+        {
+            if (!(rti_rmi_spatial_profiles_utilities->d_ensemble_statistics->Y_0_avg_computed))
+            {
+                rti_rmi_spatial_profiles_utilities->
+                    computeAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
+                        patch_hierarchy,
+                        data_context);
+            }
+            
+            if (!(rti_rmi_spatial_profiles_utilities->d_ensemble_statistics->Y_0_sq_avg_computed))
+            {
+                rti_rmi_spatial_profiles_utilities->
+                    computeMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+                        patch_hierarchy,
+                        data_context);
+            }
         }
         else
         {
@@ -384,6 +575,15 @@ FlowModelStatisticsUtilitiesFourEqnConservative::outputStatisticalQuantities(
             rti_rmi_spatial_profiles_utilities->
                 outputEnsembleAveragedMassFractionWithHomogeneityInYDirectionOrInYZPlane(
                     "Y_avg.dat",
+                    patch_hierarchy,
+                    data_context,
+                    output_time);
+        }
+        else if (statistical_quantity_key == "MASS_FRACTION_VAR")
+        {
+            rti_rmi_spatial_profiles_utilities->
+                outputEnsembleMassFractionVarianceWithHomogeneityInYDirectionOrInYZPlane(
+                    "Y_var.dat",
                     patch_hierarchy,
                     data_context,
                     output_time);
