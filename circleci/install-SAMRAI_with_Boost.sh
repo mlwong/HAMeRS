404a48fd6ebdc80ed38dcd05c8585b62dd5f5476
Man Long Wong <wongml@stanford.edu>
2022-07-24 15:36:21 -0700

    testing circleci

--- circleci/install-SAMRAI_with_Boost.sh
+++ circleci/install-SAMRAI_with_Boost.sh
@@ -8,6 +8,6 @@ cd SAMRAI-v3.11.2
 tar xvf ../v-3-11-2.tar > /dev/null 2>&1
 mkdir objs
 cd objs
-sh ../SAMRAI-v-3-11-2/configure --prefix=$SAMRAI_ROOT_WITH_BOOST --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost=$BOOST_ROOT --with-hdf5=$HDF5_ROOT
+sh ../SAMRAI-v-3-11-2/configure --prefix=$SAMRAI_ROOT_WITH_BOOST --enable-opt --with-CXX=mpicxx --with-CC=mpicc --with-F77=mpif77 --with-boost --with-hdf5=$HDF5_ROOT
 make -j 4 library
 make install
