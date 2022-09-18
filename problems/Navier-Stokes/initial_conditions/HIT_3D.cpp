bb960b0967ee7808e50708cb80072fbcaee3a376
aslangil <denis.aslangil@gmail.com>
2022-09-18 17:56:39 -0500

    Random IC is added for homogeneous turbulence.

--- problems/Navier-Stokes/initial_conditions/HIT_3D.cpp
+++ problems/Navier-Stokes/initial_conditions/HIT_3D.cpp
@@ -12,11 +12,11 @@ NavierStokesInitialConditions::initializeDataOnPatch(
 {
     NULL_USE(data_time);
     
-    if ((d_project_name != "3D HIT"))
+    if ((d_project_name != "3D HIT" && d_project_name != "3D random HIT"))
     {
         TBOX_ERROR(d_object_name
             << ": "
-            << "Can only initialize data for 'project_name' = '3D Taylor-Green vortex' !\n"
+            << "Can only initialize data for 'project_name' = '3D HIT' or '3D random HIT' !\n"
             << "'project_name' = '"
             << d_project_name
             << "' is given."
@@ -109,52 +109,67 @@ NavierStokesInitialConditions::initializeDataOnPatch(
                 }
             }
         }
-        // else if (d_project_name == "2D Taylor-Green vortex")
-        // {
-        //     // Get the dimensions of box that covers the interior of Patch.
-        //     hier::Box patch_box = patch.getBox();
-        //     const hier::IntVector patch_dims = patch_box.numberCells();
-        //     
-        //     /*
-        //      * Initialize data for a 2D inviscid Taylor-Green problem.
-        //      */
-        //     
-        //     HAMERS_SHARED_PTR<pdat::CellData<double> > density      = conservative_variables[0];
-        //     HAMERS_SHARED_PTR<pdat::CellData<double> > momentum     = conservative_variables[1];
-        //     HAMERS_SHARED_PTR<pdat::CellData<double> > total_energy = conservative_variables[2];
-        //     
-        //     double* rho   = density->getPointer(0);
-        //     double* rho_u = momentum->getPointer(0);
-        //     double* rho_v = momentum->getPointer(1);
-        //     double* E     = total_energy->getPointer(0);
-        //     
-        //     double gamma = double(5)/double(3);
-        //     
-        //     for (int j = 0; j < patch_dims[1]; j++)
-        //     {
-        //         for (int i = 0; i < patch_dims[0]; i++)
-        //         {
-        //             // Compute index into linear data array.
-        //             int idx_cell = i + j*patch_dims[0];
-        //             
-        //             // Compute the coordinates.
-        //             double x[2];
-        //             x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
-        //             x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
-        //             
-        //             // Compute density, velocities and pressure.
-        //             const double rho_i = double(1);
-        //             const double u_i = sin(x[0])*cos(x[1]);
-        //             const double v_i = -cos(x[0])*sin(x[1]);
-        //             const double p_i = double(100) + ((cos(double(2)*x[0]) + cos(double(2)*x[1])) 
-        //                                 - double(2))/double(16);
-        //             rho[idx_cell]   = rho_i;
-        //             rho_u[idx_cell] = rho_i*u_i;
-        //             rho_v[idx_cell] = rho_i*v_i;
-        //             E[idx_cell]     = p_i/(gamma - double(1)) + double(1)/double(2)*rho_i*
-        //                 (u_i*u_i + v_i*v_i);
-        //         }
-        //     }
-        // }
+        else if (d_project_name == "3D random HIT")
+        {
+            // Get the dimensions of box that covers the interior of Patch.
+            hier::Box patch_box = patch.getBox();
+            const hier::IntVector patch_dims = patch_box.numberCells();
+
+            /*
+             * Initialize data for a 3D inviscid Taylor-Green problem.
+             */
+
+            HAMERS_SHARED_PTR<pdat::CellData<double> > density      = conservative_variables[0];
+            HAMERS_SHARED_PTR<pdat::CellData<double> > momentum     = conservative_variables[1];
+            HAMERS_SHARED_PTR<pdat::CellData<double> > total_energy = conservative_variables[2];
+
+            double* rho   = density->getPointer(0);
+            double* rho_u = momentum->getPointer(0);
+            double* rho_v = momentum->getPointer(1);
+            double* rho_w = momentum->getPointer(2);
+            double* E     = total_energy->getPointer(0);
+
+            double gamma = double(5)/double(3);
+            // srand(time(NULL));
+           
+            for (int k = 0; k < patch_dims[2]; k++)
+            {
+                for (int j = 0; j < patch_dims[1]; j++)
+                {
+                    for (int i = 0; i < patch_dims[0]; i++)
+                    {   
+                        // Compute index into linear data array.
+                        int idx_cell = i +
+                            j*patch_dims[0] +
+                            k*patch_dims[0]*patch_dims[1];
+                        
+                        // Compute the coordinates.
+                        double x[3];
+                        x[0] = patch_xlo[0] + (double(i) + double(1)/double(2))*dx[0];
+                        x[1] = patch_xlo[1] + (double(j) + double(1)/double(2))*dx[1];
+                        x[2] = patch_xlo[2] + (double(k) + double(1)/double(2))*dx[2];
+                        
+                        // srand(100.0*x[0] + 55.0*x[1] + 307.0*x[2] + 68.0);
+                        // Compute density, velocities and pressure.
+                        const double rho_i = double(1);
+                        srand(100000.0*x[0]*x[1]*x[2]);
+                        const double u_i = ((double) rand() / (RAND_MAX))*2.0 - 1.0;
+                        srand(890056.0*x[0]*x[1]*x[2]);
+                        const double v_i = ((double) rand() / (RAND_MAX))*2.0 - 1.0;
+                        srand(198785.0*x[0]*x[1]*x[2]);
+                        const double w_i = ((double) rand() / (RAND_MAX))*2.0 - 1.0;
+                        
+                        const double p_i = double(100);
+                       
+                        rho[idx_cell]   = rho_i;
+                        rho_u[idx_cell] = rho_i*u_i;
+                        rho_v[idx_cell] = rho_i*v_i;
+                        rho_w[idx_cell] = rho_i*w_i;
+                        E[idx_cell]     = p_i/(gamma - double(1)) + double(1)/double(2)*rho_i*
+                            (u_i*u_i + v_i*v_i + w_i*w_i);
+                    }
+                }
+            }
+        }
     }
 }
