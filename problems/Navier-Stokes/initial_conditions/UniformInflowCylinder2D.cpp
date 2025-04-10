0df392267a58b8d0fc695b21950204a8e1b42d2f
afkula <akula@crimson.ua.edu>
2025-04-10 11:30:11 -0600

    Cleaning up the source term and initial conditions file for 2D Cylinder

--- problems/Navier-Stokes/initial_conditions/UniformInflowCylinder2D.cpp
+++ problems/Navier-Stokes/initial_conditions/UniformInflowCylinder2D.cpp
@@ -134,10 +134,7 @@ NavierStokesInitialConditions::initializeDataOnPatch(
                 spongeR = d_initial_conditions_db->getReal("spongeR");
                 spongeB = d_initial_conditions_db->getReal("spongeB");
                 spongeT = d_initial_conditions_db->getReal("spongeT");
-                // std::cout << std::setprecision(17) << u_inf << std::endl;
-                // std::cout << v_inf << std::endl;
-                // std::cout << rho_inf << std::endl;
-                // std::cout << p_inf << std::endl;
+                
             }
             
             for (int j = -num_ghosts_cons_var[1]; j < patch_dims[1] + num_ghosts_cons_var[1]; j++)
@@ -194,13 +191,10 @@ NavierStokesInitialConditions::initializeDataOnPatch(
                         p = half * (p_inf + p_ic) - half * (p_ic - p_inf)*erf((x[1]-spongeT)/(D/Real(5)));
                     }
                     
-                    // rho_u[idx_cell] = rho_inf*u;
-                    // rho_v[idx_cell] = rho_inf*v;
-                    // E[idx_cell]     = p/(gamma - Real(1)) + half*rho_inf*(u*u + v*v);
+                    rho_u[idx_cell] = rho_inf*u;
+                    rho_v[idx_cell] = rho_inf*v;
+                    E[idx_cell]     = p/(gamma - Real(1)) + half*rho_inf*(u*u + v*v);
                     
-                    rho_u[idx_cell] = rho_inf*u_inf;
-                    rho_v[idx_cell] = rho_inf*v_inf;
-                    E[idx_cell]     = p_inf/(gamma - Real(1)) + half*rho_inf*(u_inf*u_inf + v_inf*v_inf);
                 }
             }
         }
@@ -275,4 +269,3 @@ NavierStokesInitialConditions::initializeDataOnPatch(
         }
     }
 }
-
