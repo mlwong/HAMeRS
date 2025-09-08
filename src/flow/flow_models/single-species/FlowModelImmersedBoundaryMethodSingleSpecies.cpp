6111b75b91d3e03ec645d54424d5eb82bd0ddd3c
afkula <akula@crimson.ua.edu>
2025-09-08 13:48:52 -0600

    delete Real declarations (already done above)

--- src/flow/flow_models/single-species/FlowModelImmersedBoundaryMethodSingleSpecies.cpp
+++ src/flow/flow_models/single-species/FlowModelImmersedBoundaryMethodSingleSpecies.cpp
@@ -875,9 +875,9 @@ void FlowModelImmersedBoundaryMethodSingleSpecies::setConservativeVariablesCellD
                             // Second image point distance is set to d_ip_1 + 0.25*dx.
                             Real d_ip_2 = d_ip_1 + Real(0.25)*Real(dx[0]);
 
-                            Real x_ip_1 = x[0] + (dist[idx_IB] + d_ip_1)*norm_0[idx_IB];
-                            Real y_ip_1 = x[1] + (dist[idx_IB] + d_ip_1)*norm_1[idx_IB];
-                            Real z_ip_1 = x[2] + (dist[idx_IB] + d_ip_1)*norm_2[idx_IB];
+                            x_ip_1 = x[0] + (dist[idx_IB] + d_ip_1)*norm_0[idx_IB];
+                            y_ip_1 = x[1] + (dist[idx_IB] + d_ip_1)*norm_1[idx_IB];
+                            z_ip_1 = x[2] + (dist[idx_IB] + d_ip_1)*norm_2[idx_IB];
                             
                             getTrilinearInterpolationIndices3D(
                                 idx_ip_1_cons_var_LBK,
